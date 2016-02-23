ce8b408a6eddc79b19236a8a221d0ad78068f27d
Shashi Shekhar <shashi20008@gmail.com>
2016-02-24 02:38:30 +0530

    pass the app ref for module init

--- dapps.cc
+++ dapps.cc
@@ -9,14 +9,15 @@ int main(int argc, char** argv)
 	app->args = new ArgsProcessor(argc, argv);
 
 	if(app->args->has("registry")){
-		app->registry = new RegistryServer();
+		app->registry = new RegistryServer(app);
 	}
 	
 	if(app->args->has("testJSON"))
 	{
 		std::cout << "testing json" << std::endl;
-		JSON_t* _json = JSON::parse("{\"abcd\":\"efgh\",\"num\": 123456789, \"negnum\": -123, \"zero\":0,  \"key\": {\"inner\": \"innerval\"}}");
-		std::cout << "json parsed" << _json->m_type << std::endl;
+		JSON_t* _json = JSON::parse("{\"abcd\":\"efgh\", \"num\": 123456789, \"negnum\": -123, \"zero\":0,  \"key\": {\"inner\": \"innerval\", \"emptyObject\":{}}, \"float\":-123.0}");
+		std::cout << "json parsed size::" << std::endl;
+		//std::cout << _json->m_val.m_object->size() << std::endl;
 		std::cout << "JSON:: " << JSON::stringify(_json).c_str() <<std::endl;
 	}
 	return 0;
