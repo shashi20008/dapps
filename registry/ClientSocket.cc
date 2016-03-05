cf89446905c71c2d824e79de2eb95fffc52225f4
Shashi Shekhar <shashi20008@gmail.com>
2016-03-06 01:09:44 +0530

    handle errors on socket read

--- registry/ClientSocket.cc
+++ registry/ClientSocket.cc
@@ -1,11 +1,12 @@
 #include "ClientSocket.h"
 #include "../commons/utilities/StringUtils.h"
+#include "RegistryRequestProcessor.h"
 #include <map>
 
 dapps::ClientSocket::ClientSocket(dapps::RegistryServer* _registryServer) 
 {
 	m_context = new DappsContext(this);
-	m_httpSocket =  new HttpSocket(m_context, this , NULL);
+	m_httpSocket =  new HttpSocket(m_context, this , RegistryRequestProcessor::get());
 	
 	m_registryServer = _registryServer;
 	m_logger = m_registryServer->m_logger;
@@ -30,20 +31,33 @@ void dapps::ClientSocket::write(char* buffer)
 }
 
 void dapps::ClientSocket::onClientRead(uv_stream_t* _client, ssize_t nread, const uv_buf_t* buf) {
+	// EAGAIN
 	if(nread == 0)
 	{
-		std::cout << "client closed the connection. This object can safely be discarded." << std::endl;
-		// @TODO: Find if we still need to call uv_close.
+		return;
+	}
+	// EOF
+	else if (nread == UV_EOF)
+	{
+		// Client cancelled the request.
+		// @TODO: clean everything up.
+		// @TODO: cancel all pending callbacks and operations.
+		std::cout << "Client cancelled the request." << std::endl;
+		return;
+	}
+	// Error 
+	else if (nread < 0) 
+	{
+		fprintf(stderr, "error on_client_read, %s\n", uv_strerror(nread));
+		uv_close((uv_handle_t*) _client, NULL);
 		return;
 	}
 	
 	ClientSocket* _this = (ClientSocket*)((DappsContext*) _client->data)->getThis();
-	if (nread == -1) {
-		fprintf(stderr, "error on_client_read");
-		uv_close((uv_handle_t*) _client, NULL);
-		return;
-	}
 	_this->m_httpSocket->feed(buf->base, nread);
+	
+	// free the buffer space. UV won't re-use it.
+	free(buf->base);
 }
 
 void dapps::ClientSocket::allocBuffer(uv_handle_t* handle,size_t suggested_size,uv_buf_t* buf) {
