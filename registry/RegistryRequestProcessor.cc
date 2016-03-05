2a0b6778d28f568ec551f50002649a9d1a71f4ae
Shashi Shekhar <shashi20008@gmail.com>
2016-03-06 04:56:09 +0530

    parse appURI from path and get appId from mongo

--- registry/RegistryRequestProcessor.cc
+++ registry/RegistryRequestProcessor.cc
@@ -1,4 +1,6 @@
 #include "RegistryRequestProcessor.h"
+#include "../commons/exceptions/DappsException.h"
+#include "../commons/mongodb/MongoClient.h"
 #include <iostream>
 
 dapps::RegistryRequestProcessor* dapps::RegistryRequestProcessor::m_self =  NULL;
@@ -19,4 +21,21 @@ dapps::RegistryRequestProcessor* dapps::RegistryRequestProcessor::get()
 void dapps::RegistryRequestProcessor::process(dapps::DappsContext* _context, dapps::HttpSocket* socket)
 {
 	std::cout << "got to RegistryRequestProcessor::process" << std::endl;
+	std::vector<std::string> urlParts = StringUtils::split(socket->getRequestPath(), '/', 3, true);
+	std::size_t urlPartsLen = urlParts.size();
+	if(urlPartsLen < 2)
+	{
+		// @TODO: return error response instead.
+		throw DappsException("Invalid Request received");
+	}
+	
+	// @TODO: move _empty to a static constants file.
+	std::string _empty = "";
+	std::string& dappsVersion =  urlParts[0];
+	std::string& appURI = urlParts[1];
+	std::string& appURLParam = urlPartsLen > 2 ? urlParts[2] : _empty;
+	
+	MongoClient* mongoClient = new MongoClient();
+	std::string appId = mongoClient->getApplicationIdByURI(appURI.c_str());
+	std::cout << "AppId: '" << appId << "' got '" << socket->getRequestBody().str() << "' request" << std::endl;
 }
