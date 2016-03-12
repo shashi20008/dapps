3ae9acf4bf9f44c9b134de9650a0a3c89180393f
Shashi Shekhar <shashi20008@gmail.com>
2016-03-12 20:38:10 +0530

    JSON boolean parsing

--- dapps.cc
+++ dapps.cc
@@ -16,9 +16,8 @@ int main(int argc, char** argv)
 	if(app->args->has("testJSON"))
 	{
 		std::cout << "testing json" << std::endl;
-		JSON_t* _json = JSON::parse("{\"abcd\":\"efgh\", \"num\": 123456789, \"negnum\": -123, \"zero\":0,  \"key\": {\"inner\": \"innerval\", \"emptyObject\":{}}, \"float\":-123.0}");
-		std::cout << "json parsed size::" << std::endl;
-		//std::cout << _json->m_val.m_object->size() << std::endl;
+		JSON_t* _json = JSON::parse("{\"abcd\":\"efgh\", \"num\": 123456789, \"negnum\": -123, \"zero\":0,  \"key\": {\"inner\": \"innerval\", \"emptyObject\":{}}, \"float\":-123.07, \"bool\":true}");
+		std::cout << "json parsed size::" << _json->m_val.m_object->size() << std::endl;
 		std::cout << "JSON:: " << JSON::stringify(_json).c_str() <<std::endl;
 	}
 	return 0;
