a5aecb0a6b169da75801cd71673f378b0023b44e
Shashi Shekhar <shshekhar@paypal.com>
2016-04-20 10:04:31 -0700

    StringUtil::trim bug fix

--- commons/utilities/StringUtils.cc
+++ commons/utilities/StringUtils.cc
@@ -9,16 +9,20 @@ std::string dapps::StringUtils::trim(std::string strToTrim)
 	}
 	strToTrim.erase(strToTrim.begin(), sPos);
 
-	std::string::iterator ePos = strToTrim.end();
-	ePos--;
-	while((*ePos == ' ' || *ePos == '\r' || *ePos == '\t' || *ePos == '\n') && ePos != strToTrim.begin())
+	if(!strToTrim.empty()) 
 	{
+		std::string::iterator ePos = strToTrim.end();
 		ePos--;
-	};
-	if(ePos != strToTrim.end())
-	{
-		strToTrim.erase(ePos + 1, strToTrim.end());
+		while((*ePos == ' ' || *ePos == '\r' || *ePos == '\t' || *ePos == '\n') && ePos != strToTrim.begin())
+		{
+			ePos--;
+		};
+		if(ePos != strToTrim.end())
+		{
+			strToTrim.erase(ePos + 1, strToTrim.end());
+		}
 	}
+
 	return strToTrim;
 }
 
